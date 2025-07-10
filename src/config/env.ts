e312f4666fcb35ee349efc9200837d65e77f4047
ecarmona235 <ecarmona235@gmail.com>
2025-07-10 14:32:42 -0700

    Configuring env

---
+++ src/config/env.ts
@@ -0,0 +1,36 @@
+import { z } from "zod";
+import { Logger } from "@/utils/logger";
+
+const logger = new Logger("Config:Env");
+
+// Schema for environment variables
+const envSchema = z.object({
+  NODE_ENV: z.string(),
+  NEXT_PUBLIC_APP_URL: z.string(),
+});
+
+// Function to validate environment variables
+const validateEnv = () => {
+  try {
+    logger.info("Validating environment variables");
+    const env = {
+      NODE_ENV: process.env.NODE_ENV,
+    };
+    const parsed = envSchema.parse(env);
+    logger.info("Environment variables validated successfully");
+    return parsed;
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      const missingVars = error.issues.map(err => err.path.join("."));
+      logger.error("Invalid environment variables", { error: { missingVars } });
+      throw new Error(
+        `❌ Invalid environment variables: ${missingVars.join(
+          ", "
+        )}. Please check your .env file`
+      );
+    }
+    throw error;
+  }
+};
+
+export const env = validateEnv();
